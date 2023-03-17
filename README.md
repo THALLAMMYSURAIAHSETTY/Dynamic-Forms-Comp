3a4b229d1c9f24d471bef1b778dd6dc0846eabd8
THALLAMMYSURAIAHSETTY <119222831+THALLAMMYSURAIAHSETTY@users.noreply.github.com>
2023-03-17 13:56:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dynamic-Forms-Comp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-t1iseb)
---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+const DynamicFormsComp = (props) => {
+  const [formData, setFormData] = useState([
+    {
+      name: '',
+      lastName: '',
+    },
+  ]);
+  const [userDetailArr, setUserDetailArr] = useState([]);
+
+  function handleInputs(index, e) {
+    console.log(index + ' --> ' + e.target.name + ' ---> ' + e.target.value);
+    //console.log(e.target.value);
+    const values = [...formData];
+    values[index][e.target.name] = e.target.value;
+    setFormData(values);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+    //setUserDetailArr(formData);
+  }
+
+  function addElement() {
+    setFormData([...formData, { name: '', lastName: '' }]);
+  }
+  function removeElement(index) {
+    console.log(index);
+    let dataArr = [...formData];
+    dataArr.splice(index, 1);
+    setFormData(dataArr);
+  }
+
+  return (
+    <React.Fragment>
+      <p>Welcome to Dynamic component</p>
+      <form action="" onSubmit={handleSubmit}>
+        <div>
+          {formData.map((form, index) => {
+            return (
+              <div key={index}>
+                <label>
+                  Name:
+                  <input
+                    type="text"
+                    name="name"
+                    value={form.name}
+                    onChange={(event) => handleInputs(index, event)}
+                  />
+                </label>
+                <label>
+                  Last Name:
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={form.lastName}
+                    onChange={(event) => handleInputs(index, event)}
+                    />
+                  </label>
+                  {index === formData.length - 1 ? (
+                    <button onClick={addElement}>Add</button>
+                  ) : (
+                    ''
+                  )}
+                  {index > 0 ? (
+                    <button onClick={() => removeElement(index)}>Remove</button>
+                  ) : (
+                    ''
+                  )}
+                </div>
+              );
+            })}
+            <br />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+        <br />
+        <div>
+        {userDetailArr.map((detail) => {
+          return (
+            <li key={detail.id}>
+              {detail.name} || {detail.lastName}
+            </li>
+          );
+        })}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default DynamicFormsComp;  
